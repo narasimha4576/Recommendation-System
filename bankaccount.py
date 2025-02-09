9f8424409d8e3ea464c911275acb5930b5965a46
narasimha4576 <narasimha4576@gmail.com>
2025-02-09 13:27:43 +0530

    first commit

---
+++ bankaccount.py
@@ -0,0 +1,18 @@
+class BankAccount:
+    def __init__(self, account_holder, balance=0):
+        self.account_holder = account_holder
+        self.balance = balance
+    
+    def deposit(self, amount):
+        self.balance += amount
+    
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+        else:
+            print("Insufficient balance")
+
+# Creating an object
+account = BankAccount("John Doe", 1000)
+account.deposit(500)
+account.withdraw(200)
